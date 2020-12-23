1bc258fb0500aedb40361f0a91ded8df93eee195
sindu12jun <imleelijun@gmail.com>
2020-12-23 21:05:32 +1100

    chore: 讲解类型别名, Utility Type

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,7 @@
 import qs from "qs";
 import * as auth from "auth-provider";
 import { useAuth } from "context/auth-context";
+import { type } from "os";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -46,9 +47,50 @@ export const http = async (
     });
 };
 
+// JS 中的typeof，是在runtime时运行的
+// return typeof 1 === 'number'
+
+// TS 中的typeof，是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
 export const useHttp = () => {
   const { user } = useAuth();
-  // TODO 讲解 TS 操作符
+  // utility type 的用法：用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
+};
+
+// 类型别名、Utility Type 讲解
+// 联合类型
+let myFavoriteNumber: string | number;
+myFavoriteNumber = "seven";
+myFavoriteNumber = 7;
+// TS2322: Type '{}' is not assignable to type 'string | number'.
+// myFavoriteNumber = {}
+let jackFavoriteNumber: string | number;
+
+// 类型别名在很多情况下可以和interface互换
+// interface Person {
+//   name: string
+// }
+// type Person = { name: string }
+// const xiaoMing: Person = {name: 'xiaoming'}
+
+// 类型别名, interface 在这种情况下没法替代type
+type FavoriteNumber = string | number;
+let roseFavoriteNumber: FavoriteNumber = "6";
+
+// interface 也没法实现Utility type
+type Person = {
+  name: string;
+  age: number;
+};
+const xiaoMing: Partial<Person> = {};
+const shenMiRen: Omit<Person, "name" | "age"> = {};
+type PersonKeys = keyof Person;
+type PersonOnlyName = Pick<Person, "name" | "age">;
+type Age = Exclude<PersonKeys, "name">;
+
+// Partial 的实现
+type Partial<T> = {
+  [P in keyof T]?: T[P];
 };
