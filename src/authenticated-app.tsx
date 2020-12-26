4626966f7ba298712ce0faabb8b01e06c528c1ce
sindu12jun <imleelijun@gmail.com>
2020-12-26 15:58:58 +1100

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 /**
  * grid 和 flex 各自的应用场景
@@ -19,60 +20,35 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
 
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
-  grid-gap: 10rem;
 `;
 
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
---
+++ src/components/lib.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Row = styled.div<{
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + "rem"};
+  > * {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${(props) =>
+      typeof props.gap === "number"
+        ? props.gap + "rem"
+        : props.gap
+        ? "2rem"
+        : undefined};
+  }
+`;
